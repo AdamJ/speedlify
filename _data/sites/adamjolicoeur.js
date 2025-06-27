e915083282ae38cdb27342307c03d2b4b4f44463
AdamJ <adam.jolicoeur@gmail.com>
2025-06-27 12:03:48 -0400

    Clone of speedlify

---
+++ _data/sites/adamjolicoeur.js
@@ -0,0 +1,32 @@
+module.exports = {
+  name: "adamjolicoeur.com", // optional, falls back to object key
+  description: "Adam's Personal web site",
+  // skip if localhost
+  // skip if this is a new fork of the speedlify (not Zach’s)
+  skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+  options: {
+    frequency: 60 * 23, // 23 hours
+    // Use "run" if the sites don’t share assets on the same origin
+    //           and we can reset chrome with each run instead of
+    //           each site in every run (it’s faster)
+    // Use "site" if sites are all on the same origin and share assets.
+    freshChrome: "site"
+  },
+  urls: [
+    "https://www.adamjolicoeur.com/",
+    "https://www.adamjolicoeur.com/about/",
+    "https://www.adamjolicoeur.com/resume/",
+    "https://www.adamjolicoeur.com/about/",
+    "https://www.adamjolicoeur.com/designs/",
+    "https://www.adamjolicoeur.com/development/",
+    "https://www.adamjolicoeur.com/testimonials/",
+    "https://www.adamjolicoeur.com/uses/",
+    "https://www.adamjolicoeur.com/contact/",
+    "https://www.adamjolicoeur.com/credits/",
+    "https://www.adamjolicoeur.com/references/",
+    // Design pages
+    "https://www.adamjolicoeur.com/designs/alm",
+    "https://www.adamjolicoeur.com/designs/task-it",
+    "https://www.adamjolicoeur.com/designs/customer-engagement"
+  ]
+};
